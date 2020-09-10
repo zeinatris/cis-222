bd4e68c5d99ee17c9761c474424ecf6415deb434
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-09-10 18:57:04 -0400

    quiz1

---
+++ Quiz/quiz1.php
@@ -0,0 +1,47 @@
+
+/**
+ * q1.txt
+ *
+ * Quiz 1 regarding HTML and CSS
+ *
+ * @category   Quiz
+ * @package    Quiz 1
+ * @author     Chad Banks <crbanks1@hfcc.edu>
+ * @version    2020.09.10
+ * @link       X
+ */
+
+// 3 pts
+// 1. Create a basic HTML 5 page frame below.
+<html>
+    <head>
+
+// 2 pts
+// 2. Add a link tag to the HTML frame above, and name the file style.css
+// (this file does not need to really exist for this quiz)
+        <a href="style.css"></a>
+    </head>
+
+// 2 pts
+// 3. Add a div tag to the body of the page above, and fill it with any content you wish
+// Lorum ipsum is recommended
+<body>
+<div>
+<p>quiz1</p>
+</div>
+</body>
+</html>
+
+// 1 pt
+// 4. Create a PHP variable named myVar and assign any string into it.
+$myVar = 'Hi';
+
+// 1 pt
+// 5. Define inheritance below.
+Inheritance is a well-established programming principle, and PHP makes use of this principle in its object model.
+
+// 1 pt
+// 6. What are the programs you intend to use for this class? List them below.
+php
+html
+mysql
