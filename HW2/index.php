90b93dd24cbf673096b76d50f6dbc86698fe8ffa
zeinatris <zhatris@hawkmail.hfcc.edu>
2020-09-17 22:34:49 -0400

    hw2

--- HW2/index.php
+++ HW2/index.php
@@ -1,4 +1,7 @@
 <?php
+var_dump($_POST);
+
+echo '<br><br><h3>POST</h3>';
     $students = ["Chad", "Farhana", "Colin", "Ali", "Zein"];
     $arrLength = $students;
 if ($students > 5){
@@ -7,8 +10,16 @@ if ($students > 5){
 else {
     echo "Not enough students";
 }
-
-foreach ($students as $student);
-    echo $student;
+    foreach ($students as $student) ;
+        echo "$student<br>";
 
 ?>
+
+<form method="post" action="index.php" >
+
+    <label for="Message" ></label>
+    <input type="text" id="Message" name="Message" >
+
+    <input type="submit" value="Send" >
+
+</form>
