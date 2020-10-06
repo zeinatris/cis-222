c2c25dea0b523f95606e47501feb443396ae4246
Chad Banks <chadrbanks@users.noreply.github.com>
2020-10-06 17:08:52 -0400

    Update quiz3.php

--- Quiz/quiz3.php
+++ Quiz/quiz3.php
@@ -8,10 +8,10 @@
  * @package     Quiz 3
  * @author      Zein Atris <zhatris@hawkmail.hfcc.edu>
  * @version     2020.10.01
- * @grade
+ * @grade       9 / 10
  */
 
-// 3 pts
+// 2.5 / 3 pts
 // 1. You are building a new site and have to implement a few files to do so.
 //		However, there are a couple conditions for these files that must be met.
 //		Write the code needed to accomplish this task and meet the conditions.
@@ -20,7 +20,7 @@
 //		file2.php			This file imports a cool visual effect; it can be executed, but must not be executed more than one time per page.
 //		file3.php			This file dynamically generates data for the page; it can be executed as often as needed.
 require_once('apis/file1.php');
-require_once('file2.php');
+include_once('file2.php');
 include('file3.php');
 
 // 2 pts
@@ -44,7 +44,7 @@ function multiply($p1, $p2, $p3)
 {
     return $p1 * $p2 + $p3;
 }
-// 2 pts
+// 1.5 / 2 pts
 // 4. Below are a 3 variables that have been set to numbers.
 //		Call the function you defined above by passing it the three variables below, and be sure to store the result in a var.
 //		Then echo the result in a sentence, be sure the output is formatted nicely in HTML.
@@ -52,8 +52,8 @@ $i1 = 7;
 $i2 = 13;
 $i3 = 17;
 
-multiply($i1, $i2, $i3);
-echo "<p>your result is".multiply($i1, $i2, $i3), "</p>";
+$result = multiply($i1, $i2, $i3);
+echo "<p>your result is" . $result . "</p>";
 
 // 1 ex pts
 // 5. Write a for loop that executes a number of times equal to the result from question 4.
