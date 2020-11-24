e1fe0b222699a8a0b7d1ad533dc81ed81744160f
Chad Banks <chadrbanks@users.noreply.github.com>
2020-11-24 15:17:04 -0500

    Update index.php

--- Project1/index.php
+++ Project1/index.php
@@ -1,15 +1,28 @@
 <?php
 /**
- * project1
+ * index.php
  *
- * @category   SQL and PHP
- * @package    CIS-222
- * @author     Zein Atris
- * @date       2020.10.18
- * @grade
- * @comments
- * @link https://cislinux.hfcc.edu/~zhatris/cis222/Midterm/index.php
+ * Project Part I
+ *
+ * @category   	P1
+ * @package    	CIS-222
+ * @author     	Zein Atris
+ * @version    	2020.11.23
+ * @link       	https://cislinux.hfcc.edu/~zhatris/cis222/Project1/
+ *
+ * @grade		155 / 150
+ *
+ * @part1		Index driver	30 / 40
+ * @part2		Database setup	40 / 40
+ * @part3		Contact page	20 / 20
+ * @part4		Catalog page	40 / 40
+ * @part5		Appearance 		10 / 10
+ * @extra		PDO				15 / 15
+ *
+ * @comment		Link was to the midterm, I corrected it.
+ * @comment		Business logic like the contact submission should be in a handler, not in the page.
  */
+
 include('php/db.php');
 require('php/render.php');
 
